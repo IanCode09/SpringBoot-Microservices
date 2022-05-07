ea96664d694d9e275cdd8ecaf77bdb84af9a86ba
Ian Lombu <citrakemurnianlombu@gmail.com>
2022-05-07 16:10:05 +0700

    create discoveryapp and create UsersController

---
+++ discovery/src/test/java/com/woyo/photoapp/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woyo.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
